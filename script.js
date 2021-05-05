42b78491dce9f90dec124a7d708cf3295b042997
Tyler <Tyler.Pilkington@mscoding.org>
2021-05-04 23:10:27 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+﻿
+class Calculator {
+    
+    constructor() {
+        this.displayValue = "";//public attribute or class field
+    }//end constructor
+
+}//end class
+
+var Clear = new Boolean(false);
+webCalculator = new Calculator;
+
+function InputDigit(digit) {
+    if(Clear == true){
+        webCalculator.displayValue = digit
+        Clear = false
+    }else{
+        webCalculator.displayValue += digit;
+    }
+    UpdateDisplay();
+    
+
+    //console.log("pressed " + digit);
+    //console.log("displayValue is now " + webCalculator.displayValue);
+}//end function
+
+
+
+function InputOperator(operator) {
+    webCalculator.displayValue += " " + operator + " ";
+    UpdateDisplay();
+    Clear = false;
+}//end function
+
+function UpdateDisplay() {
+    //SELECT INPUT ELEMENT
+    var inputRef = document.querySelector(".calculator-screen");
+
+    //UPDATE INPUT ELEMENT
+    inputRef.value = webCalculator.displayValue;
+}//end function
+
+
+function AllClear() {
+    webCalculator.displayValue = "0";
+    Clear = true
+    UpdateDisplay();
+}//end function
+
+function Compute() {
+    let problem = webCalculator.displayValue;
+    let answer = eval(problem);
+    webCalculator.displayValue = answer;
+    UpdateDisplay();
+    Clear = true;
+}
+
+
+//TO DO complete calculator
